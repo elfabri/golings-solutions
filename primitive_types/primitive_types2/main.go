3fcc74cc6530604ad861469c61ed0827de4e0a31
elfabri <navarro.fabricio.nahuel@gmail.com>
2024-05-10 00:05:47 -0300

    ejej

--- primitive_types/primitive_types2/main.go
+++ primitive_types/primitive_types2/main.go
@@ -1,11 +1,10 @@
 // primitive_types2
 // Make me compile!
 
-// I AM NOT DONE
 package main
 
 import "fmt"
 
 func main() {
-	fmt.Printf("Hello, %s\n", who) // '%s' is used to format strings
+	fmt.Printf("Hello, %s\n", "world") // '%s' is used to format strings
 }
--- primitive_types/primitive_types3/main.go
+++ primitive_types/primitive_types3/main.go
@@ -1,12 +1,11 @@
 // primitive_types3
 // Make me compile!
 
-// I AM NOT DONE
 package main
 
 import "fmt"
 
 func main() {
 
-	fmt.Printf("Hello, I am %s and live in %s\n", who, country)
+    fmt.Printf("Hello, I am %s and live in %s\n", "Fabricio", "Arg")
 }

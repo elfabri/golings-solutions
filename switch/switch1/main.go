4602bfd4f7e0f7b3b64f453e6d2dcc0eaf269509
elfabri <navarro.fabricio.nahuel@gmail.com>
2024-05-09 23:52:33 -0300

    switchy

--- switch/switch1/main.go
+++ switch/switch1/main.go
@@ -1,17 +1,16 @@
 // switch1
 // Make me compile!
 
-// I AM NOT DONE
 package main
 
 import "fmt"
 
 func main() {
 	status := "open"
-	switch {
-	case "open":
-		fmt.Println("status is open")
-	case "closed":
-		fmt.Println("status is closed")
-	}
+    switch status {
+    case "open":
+        fmt.Println("status is open")
+    case "closed":
+        fmt.Println("status is closed")
+    }
 }

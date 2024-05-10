a64c1c84bcec19addd0a30af06015122a5ad03f0
elfabri <navarro.fabricio.nahuel@gmail.com>
2024-05-10 12:31:23 -0300

    sl

--- slices/slices1/main.go
+++ slices/slices1/main.go
@@ -1,13 +1,14 @@
 // slices1
 // Make me compile!
 
-// I AM NOT DONE
 package main
 
 import "fmt"
 
 func main() {
-	a := make(, 3, 10) // play with length and capacity
+	a := make([]int, 3, 10) // play with length and capacity
 	fmt.Println("length of 'a':", len(a))
 	fmt.Println("capacity of 'a':", cap(a))
+	fmt.Println("'a[1]':", a[1])
+	fmt.Println("'a[2]':", a[2])
 }
--- slices/slices2/main.go
+++ slices/slices2/main.go
@@ -1,13 +1,16 @@
 // slices2
 // Make me compile!
 
-// I AM NOT DONE
 package main
 
 import "fmt"
 
 func main() {
 	names := [4]string{"John", "Maria", "Carl", "Peter"}
-	lastTwoNames := names[] // after figuring out the answer, try with other low/high bounds
+    // lastTwoNames := names[] // after figuring out the answer, try with other low/high bounds
+    // lastTwoNames := names[2:]  // from idx 2 till the end
+    // lastTwoNames := names[2:4]  // from idx 2 till the idx 4-1 [2,4)
+    // firstTwoNames := names[:3]  // from idx 0 till the idx 3-1
+    lastTwoNames := names[2:]
 	fmt.Println(lastTwoNames)
 }
